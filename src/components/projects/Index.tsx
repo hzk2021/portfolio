c1e5041e9c68e71febfa80917de068b891c69cde
hzk2021 <hezhenkai1999@gmail.com>
2023-07-14 21:22:14 +0800

    Preload data

--- src/components/projects/Index.tsx
+++ src/components/projects/Index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { MutableRefObject, useEffect, useLayoutEffect, useRef, useState } from "react";
 import { IndexContext } from "../../contexts/IndexContext";
 
 import projectsInformation from "./Information";
@@ -7,14 +7,28 @@ import projectsInformation from "./Information";
 export default function Index() {
 
     const [index, setIndex] = useState(0);
-    const projectCount = projectsInformation.length;
+    const [dataLoaded, setDataLoaded] = useState(false);
+    
+    const projectInfo : MutableRefObject<JSX.Element[]> = useRef([]);
+    const projectCount : MutableRefObject<number> = useRef(0);
+
+    useEffect(() => {
+      projectInfo.current = projectsInformation;
+      projectCount.current = projectsInformation.length;
+      
+      setDataLoaded(true)
+    }, []);
 
     return (
       <>
-      <IndexContext.Provider value={{index, projectCount, setIndex}}>
-        {projectsInformation[index]}
-
-      </IndexContext.Provider>
+      {
+        dataLoaded? 
+        <IndexContext.Provider value={{index, projectCount: projectCount.current, setIndex}}>
+          {projectInfo.current[index]}
+        </IndexContext.Provider>
+        :
+        null
+      }
 
       </>
       
